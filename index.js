f1684487cbe3d0b069a11ca58f205c3e5b8d91c9
wizardywizard <littledavid175@gmail.com>
2020-02-16 16:56:56 -0800

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,14 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(array){  
+  let a = array.find(w => w.result === "W")
+  if (!!a){
+    console.log(a)
+    console.log(!!a)
+    return a.year
+  }else{
+    return undefined
+  }
+}
